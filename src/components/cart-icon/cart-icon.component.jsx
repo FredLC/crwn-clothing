3b7520e9c3406e5060d091dad97e41876ec0cde7
Fred Lefevre <fredlefevre@outlook.com>
2022-10-06 13:35:14 -0400

    Fixed typo

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,6 +1,5 @@
 import { useContext } from "react";
 import { CartContext } from "../../context/cart.context";
-import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 import { CartIconContainer, ShoppingIcon, ItemCount } from "./cart-icon.styles";
 
 const CartIcon = () => {
